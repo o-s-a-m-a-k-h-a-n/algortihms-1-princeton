572047eccb3b83789d79e9af7dadd846dd3183c9
Osama Khan <osamakhn@gmail.com>
2016-05-17 15:32:42 -0700

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Algorithms - 1 by Princeton University
+> Codebase written in Java and Ruby to learn/practise algorithms as explained in the Princeton Coursera course
---
+++ Week1/quick_find.rb
@@ -0,0 +1,19 @@
+class QuickFind
+
+  attr_accessor :id
+
+  def initialize(n)
+    self.id = Array.new(n){|index| index}
+  end
+
+  def connected?(p, q)
+    return self.id[p] == self.id[q]
+  end
+
+  def union(p,q)
+    self.id.each_index do |index|
+      id[index] = id[q] if id[index] == id[p]
+    end
+  end
+
+end
