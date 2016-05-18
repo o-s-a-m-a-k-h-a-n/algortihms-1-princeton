4cc6f8e1127726f9f406a8df6b33e509f6df4107
Osama Khan <osamakhn@gmail.com>
2016-05-18 13:09:57 -0700

    complete weightedquickunion

---
+++ Week1/weighted_quick_union.rb
@@ -0,0 +1,35 @@
+class WeightedQuickUnion
+
+  attr_accessor :id, :size
+
+  def initialize(n=10)
+    self.id = Array.new(n){|index| index}
+    self.size = Array.new(n, 1)
+  end
+
+  def connected?(p, q)
+    root(p) == root(q)
+  end
+
+  def union(p,q)
+    i = root(p)
+    j = root(q)
+    return if i==j
+    if self.size[i] < self.size[j]
+      self.id[i] = j;
+      self.size[j] += size[i];
+    else
+      self.id[j] = i;
+      self.size[i] += size[j];
+    end
+  end
+
+  private
+  def root(i)
+    while i != self.id[i] do
+      i = self.id[i]
+    end
+    i
+  end
+
+end
